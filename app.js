5889fd0c9c667e53514c315cd6e882851fd0969c
SharanyaJayaram <116339641+SharanyaJayaram@users.noreply.github.com>
2023-01-13 16:13:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ mongoose.connect(
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    //useCreateIndex: true,
+    //useFindAndModify: false,
   }
 );
 
